2d7b6817fcd5ff34f125ec3ba09b15812388e09e
Armalys <mashnert@mail.ru>
2018-11-20 19:19:27 +0300

    Lesson 9 MainRecursion changed

--- src/ru/javawebinar/basejava/MainRecursion.java
+++ src/ru/javawebinar/basejava/MainRecursion.java
@@ -5,19 +5,19 @@ import java.io.File;
 public class MainRecursion {
     public static void main(String[] args) {
         File dir = new File("../basejava/src");
-        System.out.println("Directory: " + dir.getName());
-        printDirectoryDeeply(dir);
+        String space = "";
+        printDirectoryDeeply(dir, space);
     }
 
-    private static void printDirectoryDeeply(File directory) {
+    private static void printDirectoryDeeply(File directory, String space) {
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (!file.isDirectory()) {
-                    System.out.println("\t\t" + "File: " + file.getName());
+                    System.out.println(space + "File: " + file.getName());
                 } else {
-                    System.out.println("\t" + "Directory: " + file.getName());
-                    printDirectoryDeeply(file);
+                    System.out.println(space + "Directory: " + file.getName());
+                    printDirectoryDeeply(file, space + "\t");
                 }
             }
         }
