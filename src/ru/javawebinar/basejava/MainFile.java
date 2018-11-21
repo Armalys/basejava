691d6e5f7c7daa250e6cf333bb6e8444a96b1eed
Armalys <mashnert@mail.ru>
2018-11-21 16:38:39 +0300

    Lesson 9 code formatting moving the recursion method to MainFile

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,34 +1,25 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) {
-        String filePath = "./.gitignore";
-        File file = new File(filePath);
+        File dir = new File("../basejava/src");
+        String space = "";
+        printDirectoryDeeply(dir, space);
+    }
 
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
-
-        File dir = new File("../basejava");
-        System.out.println(dir.isDirectory());
-
-        String[] list = dir.list();
-        if (list != null) {
-            for (String name : list) {
-                System.out.println(name);
+    private static void printDirectoryDeeply(File directory, String space) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (!file.isDirectory()) {
+                    System.out.println(space + "File: " + file.getName());
+                } else {
+                    System.out.println(space + "Directory: " + file.getName());
+                    printDirectoryDeeply(file, space + "\t");
+                }
             }
-        }
-
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
         }
     }
 }
