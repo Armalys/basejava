70300f0423b6240f00783851d2927912c2bc4d1b
Armalys <mashnert@mail.ru>
2018-11-29 23:45:59 +0300

    Lesson 11 DeadLock code formatting

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -5,38 +5,28 @@ public class MainDeadLock {
         final Object FIRST_OBJECT = new Object();
         final Object SECOND_OBJECT = new Object();
 
+        deadLock(FIRST_OBJECT, SECOND_OBJECT);
+        deadLock(SECOND_OBJECT, FIRST_OBJECT);
+    }
+
+
+    private static void deadLock(Object firstObject, Object secondObject) {
         new Thread(() -> {
-            synchronized (FIRST_OBJECT) {
-                System.out.println(Thread.currentThread().getName() + ": got FIRST_OBJECT monitor");
+            synchronized (firstObject) {
+                String threadName = Thread.currentThread().getName();
+                System.out.println(threadName + ": got " + secondObject + " monitor");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
-                System.out.println(Thread.currentThread().getName() + ": trying to get SECOND_OBJECT monitor");
-                synchronized (SECOND_OBJECT) {
-                    System.out.println(Thread.currentThread().getName() + ": got SECOND_OBJECT monitor");
+                System.out.println(threadName + ": trying to get " + firstObject + " monitor");
+                synchronized (secondObject) {
+                    System.out.println(threadName + ": got " + firstObject + " monitor");
                 }
             }
         }).start();
-
-        new Thread(() -> {
-            synchronized (SECOND_OBJECT) {
-                System.out.println(Thread.currentThread().getName() + ": got SECOND_OBJECT monitor");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println(Thread.currentThread().getName() + ": trying to get FIRST_OBJECT monitor");
-                synchronized (FIRST_OBJECT) {
-                    System.out.println(Thread.currentThread().getName() + ": got FIRST_OBJECT monitor");
-                }
-            }
-        }).start();
-
     }
 }
 
