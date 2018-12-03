e0ccdc2e245a395ce34c45dad25361f973665afe
Armalys <mashnert@mail.ru>
2018-12-03 21:20:38 +0300

    Lesson 12 MainStreamMethods method optimization

--- src/ru/javawebinar/basejava/MainStreamMethods.java
+++ src/ru/javawebinar/basejava/MainStreamMethods.java
@@ -8,19 +8,19 @@ import java.util.stream.IntStream;
 
 public class MainStreamMethods {
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 3, 2, 3};
-        List<Integer> integers = Arrays.asList(1, 2, 3, 3, 2, 3);
+        int[] values = {1, 1, 3, 3, 2, 3};
+        List<Integer> integers = Arrays.asList(1, 1, 3, 3, 2, 3);
 
         minValue(values);
         oddOrEven(integers);
     }
 
     private static int minValue(int[] values) {
-        return IntStream.of(values).distinct().reduce((left, right) -> left * 10 + right).getAsInt();
+        return IntStream.of(values).distinct().sorted().reduce(0, (left, right) -> left * 10 + right);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        Map<Boolean, List<Integer>> collect = integers.stream().collect(Collectors.partitioningBy(number -> number % 2 == 0));
+        Map<Boolean, List<Integer>> collect = integers.stream().collect(Collectors.partitioningBy(number -> number % 2 != 0));
         int sum = integers.stream().mapToInt(Integer::intValue).sum();
         return collect.get(sum % 2 == 0);
     }
